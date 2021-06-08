be356dcca07767f7fa50a626eb80e179390f9b00
Kanye Breast <iamthewalnutdestroyer@gmail.com>
2021-06-09 08:23:48 +1000

    added stuff

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Start from './Components/Start'
 import Main from './Components/Main'
 import Particles from './Components/Particles'
-import { SHA512 } from 'crypto-js'
+import { SHA256 } from 'crypto-js'
+import { useState } from 'react'
 
 
-function App() {
+const rand = () => {
+  return Math.floor(Math.random() * 10000000000000); 
+}
+let num = SHA256(rand().toString());
+console.log(num.toString())
+
+
+const App = () => {
+  const [main, setmain] = useState(false)
+  setTimeout(()=>{setmain(true)}, 8000);
+  
   return (
     <Router>
     <div className="App">
-      <>
+    <div className={SHA256(rand().toString().toString())} style={{width: '100vw', height: '100vh'}}>
+      <div className={num} style={{width: '100%', height: '100%'}}>
       <Start/>
 
       <Particles />
-      </>
-    <Main />
+      </div>
+    {main ? <Main /> : <></>}
+    </div>
     </div>
     </Router>
   );

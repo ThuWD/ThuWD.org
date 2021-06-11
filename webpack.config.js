3ae69c40ee17f062f98bfcc60746f6d6313bf9eb
Kanye Breast <iamthewalnutdestroyer@gmail.com>
2021-06-11 13:59:41 +1000

    empty commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,23 @@ module.exports = {
   entry: './src/index.js',
   module: {
     rules: [
-      //...
+      { 
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[sha1:hash:hex:4]'
+            },
+          },
+        ],
+      },
+
       {
         test: /\.(png|jp(e*)g|svg|gif)$/,
         use: [
@@ -14,6 +30,7 @@ module.exports = {
               name: 'images/[hash]-[name].[ext]',
             },
           },
+          
         ],
       },
     ],
